b41d48c028210ed01304e1c3253108eaed5d9bb5
Chetan Tailor <chetan.tailor@metacube.com>
2022-08-14 16:50:56 +0530

    commiting LCM of two or more numbers

--- LCM/lcm_of_morethen_two_numbers.js
+++ LCM/lcm_of_morethen_two_numbers.js
@@ -28,8 +28,8 @@ function lcm_of_array_elements(element_array)
                     counter++;
                 }
                 // Divide element_array by devisor if complete
-                // division i.e. without remainder then replace
-                // number with quotient; used for find next factor
+                // division this is  without remainder then replace
+                // number with quotient, used for find next factor
                 if (element_array[i] % divisor == 0)
                 {
                     divisible = true;
